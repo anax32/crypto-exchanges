b62171ec9da8da70faed302c21a8024dcb9b3ec1
ed <anax@hotmail.co.uk>
2020-05-06 02:00:33 +0100

    example json-log-to-csv script

---
+++ log2csv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -u
+
+#
+# get the logs from a container,
+# grep for exchange and product
+# swap quote marks ('->")
+# parse with jq into a csv file
+# write to stdout
+#
+
+# sometimes log json is badly formatted:
+#  | sed "s/'/\"/g" \
+
+docker logs coinbase \
+  | grep coinbase \
+  | grep ETH-BTC \
+  | jq -r -s '.[] | [.price, .time] | @csv'
