4521126460ba2fe5fc7bf78b4df22c116bac648d
ed <anax@hotmail.co.uk>
2020-05-24 01:45:09 +0100

    parse mongoexport to remove _id and flatten time

--- dump-data.sh
+++ dump-data.sh
@@ -1,10 +1,19 @@
 #!/bin/sh -u
 
+#
+# exports the data from mongodb.fluentdb.test
+#   removes the mongo "_id" field
+#   renames the nested "time" field
+#   compresses with gzip
+#   writes to $OUTFILE
+#
+
 OUTFILE=dump.json.gz
 
 docker exec \
   -it \
   mongo \
   /bin/sh -c "mongoexport -u=fluentd -p=test -d=fluentdb -c=test --quiet" \
+  | jq -c '. | del(._id) | . + {"timestamp": .time[]} | del(.time)' \
   | gzip \
   > ${OUTFILE}
